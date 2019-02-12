25f46bd16ac8c0207aa3d40a4b17ba8cb835a4e8
Derrick Dediino <Derrick.Dedino.2022@student.lhschools.org>
2019-02-11 23:27:51 -0500

    Demonstrating math tester with numbers and operations.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,14 +5,19 @@ public class Main {
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
-        
+        int firstNumber, secondNumber;
+
         firstWord = "dog";
         secondWord = "cat";
-
+        firstNumber = 5;
+        secondNumber = 10;
 
         System.out.println(firstWord+secondWord);
-
-
+        System.out.println("The first number + the second number=" + (firstNumber+secondNumber));
+        System.out.println("The first number - the second number=" + (firstNumber-secondNumber));
+        System.out.println("The first number * the second number=" + (firstNumber*secondNumber));
+        System.out.println("The second number / the second number=" + (secondNumber/firstNumber));
+        System.out.println("The modulus of the second number by the first=" +(secondNumber%firstNumber));
 
 
 
